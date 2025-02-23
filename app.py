65b8a774978a9d0ebcfce4423729edc9f72fd90a
Cameron Morgan <cam3r0n.m0rgan@gmail.com>
2025-02-23 13:43:01 -0600

    Changing HTML

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = ''
 app.secret_key = 'supersecretkey'
 
 
